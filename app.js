d2154e9f438412ae1072e3f3b2ea75fe15bf144b
JuaniiEtche <juanietcheverry8@gmail.com>
2023-09-11 23:31:22 -0300

    feature: Se agrega la configutracion de body-parser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const { sequelize } = require("./src/db/db");
-//Se traen los modelos para mapearlos en tablas
+// Se traen los modelos para mapearlos en tablas
 const PersonaModel = require("./src/models/Persona");
 const localidadModel = require("./src/models/Localidad");
 const proviciaModel = require("./src/models/Provincia");
@@ -9,18 +9,22 @@ const Publicacion = require("./src/models/Publicacion");
 const Servicio = require("./src/models/Servicio");
 // Se traen las rutas para gestionar los distintos recursos
 const personaRouter = require("./src/routes/personaRoute");
+const publicacionRouter = require("./src/routes/publicacionRoute");
+const bodyParser = require("body-parser");
+
 // Se crea la instancia de aplicacion express y se determina el puerto en el que va a escuchar el servidor
 const app = express();
 const port = 3000;
-// Se configuran  las asociaciones entre los modelos
-require("./src/models/associations")(sequelize);
 
 //Configuraciones propias de la instancia de la aplicacion express
 app.use(morgan("dev"));
 
+// Configura body-parser antes de definir las rutas
+app.use(bodyParser.json());
+
 // Sincroniza los modelos con la base de datos (Se utiliza en esta etapa para mapear los modelos en tablas)
 sequelize
-  .sync({ force: true })
+  .sync({ force: false })
   .then(() => {
     console.log("Modelos sincronizados con la base de datos");
   })
@@ -29,6 +33,7 @@ sequelize
   });
 
 app.use("/api", personaRouter);
+app.use("/api/", publicacionRouter);
 
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en el puerto ${port}`);
