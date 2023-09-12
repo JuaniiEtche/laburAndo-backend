1545db5a983f4d8ed1459830d0f31f3671917ce2
JuaniiEtche <juanietcheverry8@gmail.com>
2023-09-12 11:04:20 -0300

    feature: Mejora en el retorno de mensajes

--- src/controllers/publicacionController.js
+++ src/controllers/publicacionController.js
@@ -4,15 +4,16 @@ class publicacionController {
   async altaPublicacion(req, res) {
     try {
       const publicacionData = req.body;
-      const nuevaPublicacion = await Publicacion.create(publicacionData);
+      await Publicacion.create(publicacionData);
 
       res.status(201).json({
-        message: "Publicación creada con éxito",
-        publicacion: nuevaPublicacion.toJSON(),
+        Mensaje: "Publicación creada con éxito",
+        Exito: true,
       });
     } catch (error) {
-      console.error("Error al crear la publicación:", error);
-      res.status(500).json({ error: "No se pudo crear la publicación" });
+      res
+        .status(500)
+        .json({ Mensaje: "No se pudo crear la publicación", Exito: false });
     }
   }
 }
