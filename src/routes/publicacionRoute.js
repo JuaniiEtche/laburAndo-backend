108040f8cadfcf2deec2bc4209e5e9407c485da2
JuaniiEtche <juanietcheverry8@gmail.com>
2023-09-12 22:51:22 -0300

    feature: Se agregó el endpoint para devolver datos de publicaciones

--- src/routes/publicacionRoute.js
+++ src/routes/publicacionRoute.js
@@ -3,5 +3,6 @@ const publicacionController = require("../controllers/publicacionController.js")
 const publicacionRouter = express.Router();
 
 publicacionRouter.post("/publicacion", publicacionController.altaPublicacion);
+publicacionRouter.get("/publicacion", publicacionController.traerPublicaciones);
 
 module.exports = publicacionRouter;
