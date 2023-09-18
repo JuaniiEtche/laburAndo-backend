cbe031b478bfe1adc21940b599e1c16be77a9681
MatiBatista <matiasomarbatista1@gmail.com>
2023-09-18 09:43:19 -0300

    Se agrego los endpoint para obtener todos los usuarios o por id

--- src/routes/personaRoute.js
+++ src/routes/personaRoute.js
@@ -2,10 +2,22 @@
 const express = require("express");
 const personaController = require("../controllers/personaController.js");
 const personaRouter = express.Router();
-
+const Jwt = require("../models/Jwt.js");
 // Ruta para crear una nueva persona con manejo de errores
 personaRouter.post("/usuario", async (req, res, next) => {
   await personaController.altaPersona(req, res, next);
 });
+
+personaRouter.get("/usuario", Jwt.verificarToken, async (req, res, next) => {
+  await personaController.obtenerPersonas(req, res, next);
+});
+
+personaRouter.get(
+  "/usuario/:id",
+  Jwt.verificarToken,
+  async (req, res, next) => {
+    await personaController.obtenerPersona(req, res, next);
+  }
+);
 
 module.exports = personaRouter;
