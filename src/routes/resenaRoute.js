935d1d1bc23c2b8780f982788288c1fa09a28b20
MatiBatista <matiasomarbatista1@gmail.com>
2023-10-14 17:27:06 -0300

    Se agrego el endpoint para dar de alta una resenia

--- src/routes/resenaRoute.js
+++ src/routes/resenaRoute.js
@@ -1,12 +1,14 @@
-// Define las rutas relacionadas con los servicios
 const express = require("express");
 const resenaController = require("../controllers/resenaController");
 const Jwt = require("../models/Jwt.js");
 const resenaRoute = express.Router();
 
-// Ruta para obtener todos los servicios con manejo de errores
 resenaRoute.get("/resenas/:idPersona", async (req, res, next) => {
   await resenaController.obtenerResenas(req, res, next);
+});
+
+resenaRoute.post("/resenas", async (req, res, next) => {
+  await resenaController.altaResenia(req, res, next);
 });
 
 module.exports = resenaRoute;
