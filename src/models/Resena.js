a093629f97ca7a6d78c97b506959f80d352ade57
MatiBatista <matiasomarbatista1@gmail.com>
2023-10-17 21:22:50 -0300

    Se agrego el atributo estado de tipo enum al modelo de Resenia

--- src/models/Resena.js
+++ src/models/Resena.js
@@ -3,11 +3,16 @@ const { sequelize: sequelize } = require("../db/db.js");
 const Resena = sequelize.define(
   "Resena",
   {
-    calificacion: { type: DataTypes.INTEGER, allowNull: false, unique: false },
+    calificacion: { type: DataTypes.INTEGER, allowNull: true, unique: false },
     descripcion: { type: DataTypes.STRING, allowNull: true, unique: false },
     idCalificador: { type: DataTypes.INTEGER, allowNull: false, unique: false },
     idCalificado: { type: DataTypes.INTEGER, allowNull: false, unique: false },
-    fecha: { type: DataTypes.DATE, allowNull: false, unique: false },
+    fecha: { type: DataTypes.DATE, allowNull: true, unique: false },
+    estado: {
+      type: DataTypes.ENUM("pendiente", "aceptado"),
+      allowNull: false,
+      unique: false,
+    },
   },
   {
     tableName: "resena",
