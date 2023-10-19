0908380a76da5ec77c1d559e12fcfaad0d7b236f
MatiBatista <matiasomarbatista1@gmail.com>
2023-10-19 17:34:22 -0300

    Se agregaron las rutas para get resenias aceptadas y pendientes

--- src/routes/resenaRoute.js
+++ src/routes/resenaRoute.js
@@ -7,6 +7,25 @@ resenaRoute.get("/resenas/:idPersona", async (req, res, next) => {
   await resenaController.obtenerResenas(req, res, next);
 });
 
+resenaRoute.get("/resenas", async (req, res, next) => {
+  if (req.query.idCalificador) {
+    await resenaController.obtenerResenasPendientesPorCalificador(
+      req.query.idCalificador,
+      res,
+      next
+    );
+  } else if (req.query.idCalificado) {
+    await resenaController.obtenerResenasAceptadasPorCalificado(
+      req.query.idCalificado,
+      res,
+      next
+    );
+  } else {
+    res.send("Todos");
+    //Devolver todos
+  }
+});
+
 resenaRoute.post("/resenas", async (req, res, next) => {
   await resenaController.altaResenia(req, res, next);
 });
